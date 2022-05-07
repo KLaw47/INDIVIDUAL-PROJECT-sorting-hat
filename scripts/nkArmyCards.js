d97caf1e281795d948a7dded893d6d1bbedfba67
Keaton Law <keaton.c.law@gmail.com>
2022-05-06 20:56:55 -0500

    added nkimage

--- scripts/nkArmyCards.js
+++ scripts/nkArmyCards.js
@@ -5,7 +5,7 @@ const renderArmy = (array) => {
 
     domString += 
     `<div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="./images/beast.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${member.name}</h5>
       <p>Servant of our Lord of Darkness</p>
